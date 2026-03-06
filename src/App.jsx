be3258a29ffc929bb9b669130ff4ada76bcc0777
Rajnish Kumar <rajnish09032003@gmail.com>
2026-03-06 22:40:06 +0530

    Page speed error solved try 1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ScrollTop from "./components/ScrollTop";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
